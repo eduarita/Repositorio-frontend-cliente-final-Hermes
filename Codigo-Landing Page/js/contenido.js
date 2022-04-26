dff806e54251c81ffd40e0e23376020ea3a92d32
EduardoCastro26 <93845556+EduardoCastro26@users.noreply.github.com>
2022-04-25 18:18:53 -0600

    Add files via upload

---
+++ Codigo-Landing Page/js/contenido.js
@@ -0,0 +1,32 @@
+var section = [ 
+    {
+        id: 1,
+        imagen: 'img/hermes.png',
+        titulo: 'Confiable y seguro',
+        parrafo:'Hermes cuenta con mensajeros de confianza que te ayudaran a llevar tu pedido donde quiera que estes'
+    },
+    {
+        id: 2,
+        imagen: 'img/mapa.png',
+        titulo: '¡Entrega a domicilio!',
+        parrafo:'Realiza tu pedido en cualquier lugar que te encuentres.'
+    },
+    {
+        id: 3,
+        imagen: 'img/entrega-de-comida.png',
+        titulo: 'Pide tu comida favorita',
+        parrafo:'Encuentra tu restaurante de preferencia y realiza tu orden'
+    },
+    {
+        id: 4,
+        imagen: 'img/homework.png',
+        titulo: 'Invierte tu tiempo en otras actividades',
+        parrafo:'Deja que Hermes se encargue de llevar tus pedidos'
+    },
+    {
+        id: 5,
+        imagen: 'img/fast-delivery.png',
+        titulo: 'Entregas al instante',
+        parrafo:'Aqui en Hermes nos preocupamos por que llegue lo mas pronto posible'
+    }
+]
